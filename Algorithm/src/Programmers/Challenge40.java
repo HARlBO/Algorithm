12cffe301e47543b719697bccf34cab81350f946
Jinha2 <j_2j@naver.com>
2017-12-06 14:33:16 +0900

    Programmers Challenge 40번 문

--- Algorithm/src/Programmers/Challenge40.java
+++ Algorithm/src/Programmers/Challenge40.java
@@ -31,7 +31,6 @@ public class Challenge40 {
 
 	public static void main(String args[]) {
 		Challenge40 expressions = new Challenge40();
-		// 아래는 테스트로 출력해 보기 위한 코드입니다.
 		System.out.println("답 : "+expressions.expressions(15));
 	}
 }
