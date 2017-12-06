ca772fb096b272f170d0d4c09536bbca3db18de1
Jinha2 <j_2j@naver.com>
2017-12-06 14:23:44 +0900

    Programmers Challenge 40번 문제

---
+++ Algorithm/src/Programmers/Challenge40.java
@@ -0,0 +1,37 @@
+package Programmers;
+/**
+ * 
+ * 숫자의 표현 
+ * https://programmers.co.kr/learn/challenge_codes/156
+ * 
+ */
+public class Challenge40 {
+	public int expressions(int num) {
+		int answer = 0;
+		
+		for (int i = 1; i <= num/2; i++) {
+			int sum = 0;
+			for (int j = i; j <= num; j++) {
+				if (sum == num) {
+					answer ++;
+					break;
+				} else if (sum < num){
+					sum = sum + j;
+					System.out.println(sum);
+				} else {
+					break;
+				}
+			}
+		}
+		
+		answer++;
+		
+		return answer;
+	}
+
+	public static void main(String args[]) {
+		Challenge40 expressions = new Challenge40();
+		// 아래는 테스트로 출력해 보기 위한 코드입니다.
+		System.out.println("답 : "+expressions.expressions(15));
+	}
+}
